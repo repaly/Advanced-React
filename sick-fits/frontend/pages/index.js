45ea6d50a9811e96c0d57b086afee573ebaa7208
Dmitry <dimsemin2008@yandex.ru>
2019-05-14 17:47:44 +0300

    boilerplate

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <p>Hey i am home component!</p>
+  </div>
+)
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <p>Hey i am sell component!</p>
+  </div>
+)
+
+export default Sell;
